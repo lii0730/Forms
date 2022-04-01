02f586055bc60eeede1b8620d4bbda6b0e908ad9
LeeHsss <lii0730@icloud.com>
2022-04-01 16:31:39 +0900

    Initial Commit

---
+++ Forms/FormsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormsApp.swift
+//  Forms
+//
+//  Created by LeeHsss on 2022/04/01.
+//
+
+import SwiftUI
+
+@main
+struct FormsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
